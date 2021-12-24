fccf18d12e89cc5c2c112e5cfd0b3e41bd29c2d5
hezhanghe <hezhanghe@huami.com>
2021-12-24 19:46:21 +0800

    feat |> 增加 WebViewManager.kt

--- webmodule/src/main/java/com/huami/webmodule/WebViewManager.kt
+++ webmodule/src/main/java/com/huami/webmodule/WebViewManager.kt
@@ -6,10 +6,12 @@ import android.content.MutableContextWrapper
 import android.graphics.Color
 import android.os.Build
 import android.os.Looper
+import android.view.ViewGroup
 import com.tencent.smtt.export.external.interfaces.IX5WebSettings
 import com.tencent.smtt.sdk.CookieManager
 import com.tencent.smtt.sdk.WebSettings
 import com.tencent.smtt.sdk.WebView
+import java.lang.Exception
 
 /**
  * @date 2021/12/23
@@ -55,6 +57,7 @@ class WebViewManager private constructor() {
             instance().destroy()
         }
     }
+
     private val webViewCache = ArrayList<WebView>(1)
 
     private fun obtain(context: Context): WebView {
@@ -101,10 +104,8 @@ class WebViewManager private constructor() {
             this.displayZoomControls = false
             this.useWideViewPort = true
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                this.mixedContentMode = android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-                CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
-            }
+            this.mixedContentMode = android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
         }
         webView.settingsExtension?.apply {
             setContentCacheEnable(true)
@@ -114,12 +115,42 @@ class WebViewManager private constructor() {
     }
 
     private fun recycle(webView: WebView) {
-        // TODO: 2021/12/23 add recycle
+        try {
+            webView.let {
+                it.stopLoading()
+//    清除页面内容，导致复用时加载栈底就是这个空白页面，所以在返回时需要对栈底进行判断，如果为空则直接返回
+                it.loadDataWithBaseURL(null, "", "text/html", "utf-8", null)
+                it.clearHistory()
+                it.pauseTimers()
+                it.webChromeClient = null
+                it.webViewClient = null
+                it.parent?.let { parent ->
+                    (parent as ViewGroup).removeView(it)
+                }
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            if (webViewCache.contains(webView).not()) {
+                webViewCache.add(webView)
+            }
+
+        }
     }
 
+    //需要先调用recycle
+    private fun destroy() {
+        try {
+            webViewCache.forEach {
+                it.removeAllViews()
+                it.destroy()
+                webViewCache.remove(it)
+            }
 
-    private fun destroy(){
-        // TODO: 2021/12/23 add destroy
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 
