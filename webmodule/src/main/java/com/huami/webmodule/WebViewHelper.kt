8acb20cded11cac5e5a98ba20aab295378793b41
hezhanghe <hezhanghe@huami.com>
2021-12-24 19:50:56 +0800

    feat |> 增加 WebViewHelper.kt

---
+++ webmodule/src/main/java/com/huami/webmodule/WebViewHelper.kt
@@ -0,0 +1,12 @@
+package com.huami.webmodule
+
+import android.view.ViewGroup
+
+/**
+ * @date 2021/12/24
+ * @author hezhanghe@zepp.com
+ * @desc
+ */
+class WebViewHelper private constructor(parent: ViewGroup) {
+
+}
