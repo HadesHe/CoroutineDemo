8ffa51be42cbdbcdbc45d515d36dc54bd6f4621d
hezhanghe <hezhanghe@huami.com>
2022-03-31 15:32:04 +0800

    refact |> refact settings.gradle -> settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.aliyun.com/nexus/content/groups/public/")
+        maven("https://maven.aliyun.com/nexus/content/repositories/jcenter")
+        maven("https://maven.aliyun.com/nexus/content/repositories/google")
+        maven("https://maven.aliyun.com/nexus/content/repositories/gradle-plugin")
+
+        maven("https://jitpack.io")
+
+    }
+}
+rootProject.name = "CoroutineDemo"
+include(":app")
+include(":webmodule")
+include(":jetpacksourcemodule")
