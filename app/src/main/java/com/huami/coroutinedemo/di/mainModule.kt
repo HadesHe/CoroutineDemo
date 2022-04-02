5921f3da79a2e74adad44314d80714115bcdc8ff
hezhanghe <hezhanghe@huami.com>
2022-04-02 11:25:25 +0800

    feat |> 增加 koin 主模块

--- app/src/main/java/com/huami/coroutinedemo/di/mainModule.kt
+++ app/src/main/java/com/huami/coroutinedemo/di/mainModule.kt
@@ -1,15 +1,21 @@
 package com.huami.coroutinedemo.di
 
 import com.huami.coroutinedemo.MainViewModel
+import com.huami.coroutinedemo.coroutine.CustomCoroutine
 import com.huami.coroutinedemo.flow.FlowViewModel
+import kotlinx.coroutines.CoroutineScope
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import org.koin.java.KoinJavaComponent
 
 /**
  * @date 2021/12/10
  * @author hezhanghe@zepp.com
  * @desc
  */
+
+internal const val MAIN_SCOPE = "MAIN_SCOPE"
 val mainModule = module {
 
     viewModel {
@@ -19,4 +25,13 @@ val mainModule = module {
     viewModel {
         FlowViewModel()
     }
+
+    scope(named(MAIN_SCOPE)) {
+        scoped<CoroutineScope> {
+            CustomCoroutine()
+        }
+    }
 }
+
+fun getMainScope() = KoinJavaComponent.getKoin()
+    .getOrCreateScope(MAIN_SCOPE, named(MAIN_SCOPE))
